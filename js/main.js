27056f5844182c70e159d3b2e8f50622c7d318ab
elismountain <echospb@mail.ru>
2020-10-20 21:38:33 +0100

    fixed mistakes

--- js/main.js
+++ js/main.js
@@ -127,8 +127,8 @@ var fillCard = function (element, mapCard) {
     }
   };
 
- var popupPhotos = mapCard.querySelector(`.popup__photos`);
- popupPhotos.appendChild(createPhotosFragment(element.offer.photos));
+  var popupPhotos = mapCard.querySelector('.popup__photos');
+  popupPhotos.appendChild(createPhotosFragment(element.offer.photos));
 
   mapCard.querySelector('.popup__title').textContent = element.offer.title;
   mapCard.querySelector('.popup__text--address').textContent = element.offer.address;
